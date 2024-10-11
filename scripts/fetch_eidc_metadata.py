2e99516dbd18d4259663b7609d158954e08f3c34
mpc <matthewcoole@gmail.com>
2024-10-11 11:28:41 +0100

    Cleaned pipeline

---
+++ scripts/fetch_eidc_metadata.py
@@ -0,0 +1,25 @@
+import requests
+import json
+from argparse import ArgumentParser
+
+URL = "https://catalogue.ceh.ac.uk/eidc/documents"
+
+def main(output_file: str) -> None:
+    res = requests.get(
+        URL,
+        headers={"content-type": "application/json"},
+        params={
+            "page": 1,
+            "rows": 2000,
+            "term": "recordType:Dataset",
+        },
+    )
+    with open(output_file, "w") as f:
+        json.dump(res.json(), f)
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser("fetch_eidc_metadata.py")
+    parser.add_argument("output", help="The file path to save the downloaded data to.")
+    args = parser.parse_args()
+    main(args.output)
---
+++ scripts/prepare_data.py
@@ -0,0 +1,40 @@
+from typing import List, Dict
+import json
+from argparse import ArgumentParser
+
+
+METADATA_FIELDS = ["title", "description", "lineage"]
+
+
+def extact_eidc_metadata_fields(json_data: Dict, fields: List[str] = METADATA_FIELDS) -> Dict[str,str]:
+    metadata = {}
+    metadata["id"] = json_data["identifier"]
+    for field in fields:
+        if json_data[field]:
+            metadata["field"] = field
+            metadata["value"] = json_data[field]
+    return metadata
+
+
+def parse_eidc_metadata(file_path: str) -> List[Dict[str,str]]:
+    data = []
+    with open(file_path) as f:
+        json_data = json.load(f)
+        for dataset in json_data["results"]:
+            dataset_metadata = extact_eidc_metadata_fields(dataset)
+            data.append(dataset_metadata)
+    return data
+
+
+def main(input, output) -> None:
+    data = parse_eidc_metadata(input)
+    with open(output, "w") as f:
+        json.dump(data, f, indent=4)
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser("prepare_data.py")
+    parser.add_argument("input", help="The file to be used as input.")
+    parser.add_argument("output", help="The path to save the processed result.")
+    args = parser.parse_args()
+    main(args.input, args.output)
