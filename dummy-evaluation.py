68e17ac4ce10df582ffa0b941d5c787178f4eab8
mpc <matthewcoole@gmail.com>
2024-10-18 14:02:56 +0100

    Reformatted code with ruff

--- dummy-evaluation.py
+++ dummy-evaluation.py
@@ -3,7 +3,12 @@ import pandas as pd
 import plotly.graph_objects as go
 import plotly.io as pio
 
-metrics = {"answer_relevancy", "answer_correctness", "context_precision", "context_recall"}
+metrics = {
+    "answer_relevancy",
+    "answer_correctness",
+    "context_precision",
+    "context_recall",
+}
 dummy_data = {metric: np.random.rand(100) for metric in metrics}
 df = pd.DataFrame(dummy_data)
 
@@ -13,8 +18,20 @@ with open("metrics.txt", "w") as f:
 
 pio.templates.default = "gridon"
 fig = go.Figure()
-metrics = [metric for metric in df.columns.to_list() if metric not in ["question", "ground_truth", "answer", "contexts"]]
+metrics = [
+    metric
+    for metric in df.columns.to_list()
+    if metric not in ["question", "ground_truth", "answer", "contexts"]
+]
 for metric in metrics:
-    fig.add_trace(go.Violin(y=df[metric], name=metric, points="all", box_visible=True, meanline_visible=True))
-fig.update_yaxes(range=[-0.02,1.02])
+    fig.add_trace(
+        go.Violin(
+            y=df[metric],
+            name=metric,
+            points="all",
+            box_visible=True,
+            meanline_visible=True,
+        )
+    )
+fig.update_yaxes(range=[-0.02, 1.02])
 fig.write_image("metrics.png")
