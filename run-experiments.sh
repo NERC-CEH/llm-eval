e2f8fa877e6b4314b2ff577e4073f80cce1342da
mpc <matthewcoole@gmail.com>
2024-12-20 09:25:34 +0000

    adds uv support in experiments script

--- run-experiments.sh
+++ run-experiments.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 NC='\033[0m'
 GREEN='\033[0;32m'
-dvc queue remove --all
+uv run dvc queue remove --all
 models=("llama3 llama3.1 mistral-nemo")
 for model in $models
 do
-    dvc exp run --queue -S rag.model=$model
+    uv run dvc exp run --queue -S rag.model=$model -S sub-sample=1 -S max-length=250 -S test-set-size=5
 done
-dvc queue start
-dvc queue status
-echo -e "Run ${GREEN}dvc queue status${NC} to check the state of the experiments"
+uv run dvc queue start
+uv run dvc queue status
+echo -e "Use ${GREEN}uv run dvc queue status${NC} to check the state of the experiments"
